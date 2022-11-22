cc430a812e5d0a917f757074a685644df6054b44
MatteoCarbone17 <matteocarbone17@live.com>
2022-11-22 17:42:03 +0100

    maybe it was fixed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 
 const button = document.querySelector('div.buttons a.btn');
 
-let distance = document.getElementById('input#km');
+let distance = document.getElementById('km');
 
-let age = document.getElementById('input#age');
+let age = document.getElementById('age');
 
 
 
 button.addEventListener('click', function (){
 
     let price;
-    price = parseFloat(distance.value * 0.2762);
+    price = parseFloat(km.value * 0.2762);
 
     if ( age.value < 18) {
         price = parseFloat( price - 0.17);
+        document.getElementById('output') .innerHTML = 'Il costo del biglietto è' + ' ' + price.toFixed(2) + '&euro;'
     } else if ( age.value > 65) {
         price = parseFloat(price - 0.33);
+        document.getElementById('output') .innerHTML = 'Il costo del biglietto è' + ' ' + price.toFixed(2) + '&euro;'
     } else {
     document.getElementById('output') .innerHTML = 'Il costo del biglietto è' + ' ' + price.toFixed(2) + '&euro;'
     }
